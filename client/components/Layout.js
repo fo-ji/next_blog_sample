e44da870f33223ba7ba3603e1d908488e6e2f39e
fo-ji <fumiya1753@gmail.com>
2022-08-11 14:47:23 +0900

    add styles

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,17 +1,19 @@
 import Head from 'next/head'
+import styles from './layout.module.css'
+import utilsStyles from '../styles/utils.module.css'
 
 const name = 'Shin Code'
 export const siteTitle = 'Next.js Blog'
 
 function Layout({ children }) {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <link rel="icon" href="favicon.ico" />
       </Head>
-      <header>
-        <img src="/images/profile.png" />
-        <h1>{name}</h1>
+      <header className={styles.header}>
+        <img src="/images/profile.png" className={utilsStyles.borderCircle} />
+        <h1 className={utilsStyles.heading2Xl}>{name}</h1>
       </header>
       <main>{children}</main>
     </div>
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,78 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import utilStyles from '../styles/utils.module.css'
 
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
 export default function Home() {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <section className={utilStyles.headingMd}>
+        <p>
+          私はフルスタックエンジニアです/Udemy講師として活動しています/好きな言語はJavascriptです
+        </p>
+        {/* <Link href="/posts/first-post">最初の投稿はこちら</Link>  あとで外す*/}
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>📝エンジニアのブログ</h2>
+        <div className={`${styles.grid}`}>
+          <article>
+            <Link href={'/'}>
+              <img
+                src="/images/thumbnail01.jpg"
+                className={`${styles.thumbnailImage}`}
+              />
+            </Link>
+            <Link href={'/'}>
+              <a className={utilStyles.boldText}>title</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>2022, Febrary 20</small>
+          </article>
+          <article>
+            <Link href={'/'}>
+              <img
+                src="/images/thumbnail01.jpg"
+                className={`${styles.thumbnailImage}`}
+              />
+            </Link>
+            <Link href={'/'}>
+              <a className={utilStyles.boldText}>title</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>2022, Febrary 20</small>
+          </article>
+          <article>
+            <Link href={'/'}>
+              <img
+                src="/images/thumbnail01.jpg"
+                className={`${styles.thumbnailImage}`}
+              />
+            </Link>
+            <Link href={'/'}>
+              <a className={utilStyles.boldText}>title</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>2022, Febrary 20</small>
+          </article>
+          <article>
+            <Link href={'/'}>
+              <img
+                src="/images/thumbnail01.jpg"
+                className={`${styles.thumbnailImage}`}
+              />
+            </Link>
+            <Link href={'/'}>
+              <a className={utilStyles.boldText}>title</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>2022, Febrary 20</small>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
