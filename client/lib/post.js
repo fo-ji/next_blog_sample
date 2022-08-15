ffe667ab9cbd2bd0cfd0d97e29e9a2bafa95a8dc
fo-ji <fumiya1753@gmail.com>
2022-08-15 13:47:19 +0900

    add md and post.js

---
+++ client/lib/post.js
@@ -0,0 +1,24 @@
+import path from 'path'
+import fs from 'fs'
+import matter from 'matter'
+
+const postsDirectory = path.join(process.cwd(), 'posts')
+
+// mdファイルを取り出す
+export function getPostsData() {
+  const fileNames = fs.readdirSync(postsDirectory)
+  const allPostdData = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.md$/, '') // ファイル名(ID)
+
+    // マークダウンファイルを文字列として読み取る
+    const fullPath = path.join(postsDirectory, fileName)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    const matterResult = matter(fileContents)
+
+    // IDとデータを返す
+    return {
+      id,
+      ...matterResult,
+    }
+  })
+}
